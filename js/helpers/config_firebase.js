4e18faf487f2731a034659de31fddb9150746328
Jennifercarmen <jennifercarmens@gmail.com>
2018-01-16 11:20:27 -0500

    facebook y google funcional

---
+++ js/helpers/config_firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDnggxgl0EzrkUmptl9EUcbwfCbOQlnsmg",
+  authDomain: "neoxcode-8bd83.firebaseapp.com",
+  databaseURL: "https://neoxcode-8bd83.firebaseio.com",
+  projectId: "neoxcode-8bd83",
+  storageBucket: "neoxcode-8bd83.appspot.com",
+  messagingSenderId: "1093799281344"
+};
+firebase.initializeApp(config);
