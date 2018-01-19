85812d2848fa109be045d06af0074e64a094674e
Jennifecarmen <jennifercarmens@gmail.com>
2018-01-19 02:57:48 -0500

    user, busqueda, favoritos

--- js/helpers/functions.js
+++ js/helpers/functions.js
@@ -1,10 +1,27 @@
-function sessionActive() {
+jQuery(document).ready(function () {
+  function sessionActive() {
   firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
       console.log('sesion activa');
     } else {
-      location.href = "../index.html";
+      location.href = "../views/main.html";
     }
   });
 }
 sessionActive();
+
+  var $imageUser = $('#userphoto');
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      // User is signed in.
+      var displayName = user.displayName;
+      var photoURL = user.photoURL;
+      $imageUser.attr('src', photoURL);
+      // ...
+    } else {
+      // User is signed out.
+      console.log("No ha iniciado sesion");
+    }
+  });
+});
--- js/perfil.js
+++ js/perfil.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function () {
     if (user) {
       // User is signed in.
       var displayName = user.displayName;
+      console.log(displayName);
       var photoURL = user.photoURL;
       var uid = user.uid;
       var providerData = user.providerData;
@@ -14,7 +15,7 @@ jQuery(document).ready(function () {
       // ...
     } else {
       // User is signed out.
-      // ...
+      console.log("No ha iniciado sesion");
     }
   });
   $(".oculto").hide();
@@ -30,4 +31,78 @@ jQuery(document).ready(function () {
       return false;
     }
   });
+
+  function viewFavorites() {
+    firebase.auth().onAuthStateChanged(function (user) {
+
+        firebase.database().ref('Moviessen').on('child_added', function (snapshot) {
+            var html = '';
+            var key = snapshot.key;
+            var mensaje = snapshot.val().message;
+            var fecha = snapshot.val().fecha;
+            var hora = snapshot.val().hora;
+            var uid = snapshot.val().uid;
+            var ruta = snapshot.val().images;
+            var rut=JSON.stringify(ruta, null, ' ');
+            var contact = JSON.parse(rut);
+            var keyimage=(Object.keys(contact));
+            var obth=Object.values(contact[keyimage]);
+            var url=obth[1];           
+            firebase.database().ref('/Usuarios/' + uid).on('value', snap => {
+                name = snap.val().name;
+                photoURL = snap.val().photoURL;                
+                html += '<div class="post">' +
+                    '<div class="tweet-je">' +
+                    '<div class="row">' +
+                    '<div class="col s3">' +
+                    '<img class="responsive-img profile-img" id="img-user" src=' + photoURL + ' alt="">' +
+                    '</div>' +
+                    '<div class="col s9">' +
+                    '<span id="name_user bold">' + name + '</span>' +
+                    '<br>' +
+                    '<span class="fecha_post">' +
+                    fecha + ' - ' + hora +
+                    '</span>' +
+                    '</div>' +
+                    '</div>' +
+                    '<div class="row">' +
+                    '<div class="col s12"> ' +
+                    '<p id="postdesc"> ' +
+                    mensaje +
+                    '</p>' +
+                    '</div>' +
+                    '</div>' +
+                    '<div class="row">' +
+                    '<div class="col s12"><img class="responsive-img" src='+ url +'></div>' +
+                   
+                    '</div>' +
+                    '<div class="row">' +
+                    '<div class="col s4"><i  class="tiny material-icons icon_post">favorite_border</i></div>' +
+                    '<div class="col s4"><i data-key='+ key +' class="tiny material-icons icon_post comment">message</i></div>' +
+                    '<div class="col s4"><i class="tiny material-icons icon_post">share</i></div>' +
+                    '</div>' +
+               
+                    '</div>' +
+                    '</div>/';
+
+
+                $boxPost.append(html);
+            });
+            $('.comment').on('click', function () {
+                $('textarea').val('');
+                $(keycomment).val('');
+
+                var keycomment = $(this).data('key');
+                console.log(keycomment);
+                $('#modal3').modal('open');
+                $('#commentpost').on('click', function () {
+                    console.log(keycomment);
+                    $(keycomment).remove();
+                });
+            });
+        });
+})
+  }
+viewFavorites();
+
 });
