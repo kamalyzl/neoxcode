964577b9bcb39b7046758c8b906855cc7ae07755
Jennifercarmen <jennifercarmens@gmail.com>
2018-01-22 09:20:30 -0500

    mejorando seccion favoritos

--- js/app.js
+++ js/app.js
@@ -1,93 +1,90 @@
-  (function(){
-    init();
-function init(){
-    var inputFld=$('#titleFld');
-    var searchBtn=$('#searchBtn');
+(function() {
+  init();
+  function init() {
+    var inputFld = $('#titleFld');
+    var searchBtn = $('#searchBtn');
     searchBtn.click(handleSearchBtn);
     function handleSearchBtn() {
-        var title = inputFld.val();
-        console.log(title);
-        var url = 'https://www.omdbapi.com/?&apikey=8ad4c53d&s=' + encodeURI(title) ;
-        $.ajax({
-          url: url,
-          success: renderMovies
-        });
-      };
-}
-function renderMovies(response) {
+      var title = inputFld.val();
+      console.log(title);
+      var url = 'https://www.omdbapi.com/?&apikey=8ad4c53d&s=' + encodeURI(title) ;
+      $.ajax({
+        url: url,
+        success: renderMovies
+      });
+    };
+  }
+  function renderMovies(response) {
     console.log(response);
-    var movieList=  $('#mobileList');
+    var movieList = $('#mobileList');
     
     movieList.empty();
     for (var i in response.Search) {
-
       var movie = response.Search[i];
       var li = $('<li class="list-group-item">');
-      var span=$('<span>');
-      var img=$('<img  id="poster" src="' + movie.Poster + '" width="120px">'); 
+      var span = $('<span>');
+      var img = $('<img  id="poster" src="' + movie.Poster + '" width="120px">'); 
       li.append(img);
-      li.append('<span>'+ movie.Title +'</span>');
-      span.append('<span>'+ movie.imdbID +'</span>');
-      span.append('<span>'+ movie.Year +'</span>');
-      li.append('<button  data-imdb='+ movie.imdbID +' data-poster='+ movie.Poster +' data-year='+ movie.Year +'  data-Title='+ movie.Title +'  type="button" class="btn btn-fav">favoritos</button>');
+      li.append('<span>' + movie.Title + '</span>');
+      span.append('<span>' + movie.imdbID + '</span>');
+      span.append('<span>' + movie.Year + '</span>');
+      li.append('<button  data-imdb=' + movie.imdbID + ' data-poster=' + movie.Poster + ' data-year=' + movie.Year + '  data-Title=' + movie.Title + '  type="button" class="btn btn-fav btn-danger">favoritos</button>');
       movieList.append(li);
-    movieList.append(span);
-
+      movieList.append(span);
     }
-    //guardar las peliculas vistas por el usuario en firebase
-    $('.btn-fav').click(function(){
-        var imdbID = $(this).data('imdb');
-        var titleMovie = $(this).data('title');
-          var posterMovie =  $(this).data('poster'); 
-          var ratingMovie =  $(this).data('year');
-        console.log(imdbID);
-        console.log(titleMovie);
-        console.log(posterMovie);
-        console.log(ratingMovie);
-        firebase.auth().onAuthStateChanged(function (user) {
-            var uid = user.uid;
-            Addmoviesseen( imdbID,uid,titleMovie, posterMovie, ratingMovie);
-          });
-        });
-}
+    // guardar las peliculas vistas por el usuario en firebase
+    $('.btn-fav').click(function() {
+      var imdbID = $(this).data('imdb');
+      var titleMovie = $(this).data('title');
+      var posterMovie = $(this).data('poster'); 
+      var ratingMovie = $(this).data('year');
+      console.log(imdbID);
+      console.log(titleMovie);
+      console.log(posterMovie);
+      console.log(ratingMovie);
+      firebase.auth().onAuthStateChanged(function(user) {
+        var uid = user.uid;
+        Addmoviesseen(imdbID, uid, titleMovie, posterMovie, ratingMovie);
+      });
+    });
+  }
 
 
-function Addmoviesseen( imdbID,uid,titleMovie, posterMovie, ratingMovie) {
+  function Addmoviesseen(imdbID, uid, titleMovie, posterMovie, ratingMovie) {
     console.log(uid);
     console.log(imdbID);
     console.log(titleMovie);
     console.log(posterMovie);
     console.log(ratingMovie);
-    firebase.database().ref('Moviessen/' + uid +'/'+ imdbID).set({
-        Title:titleMovie,
-        posterMovie:posterMovie,
-        ratingImdb:ratingMovie
-      });
-}
-    
-    var searchBtn = $('#searchBtn');
-
-    searchBtn.on('click', function (event) {
-      var inputFld = $('#titleFld').val();
-      localStorage.setItem("title", inputFld);
-      var title = localStorage.getItem("title");
-      console.log(title);
-      window.location.href = 'movies.html';
+    firebase.database().ref('Moviessen/' + uid + '/' + imdbID).set({
+      Title: titleMovie,
+      posterMovie: posterMovie,
+      ratingImdb: ratingMovie
     });
+  }
+    
+  var searchBtn = $('#searchBtn');
 
+  searchBtn.on('click', function(event) {
+    var inputFld = $('#titleFld').val();
+    localStorage.setItem('title', inputFld);
+    var title = localStorage.getItem('title');
+    console.log(title);
+    window.location.href = 'movies.html';
+  });
 })();
 function singOff() {
-    window.location.href = '../index.html';
-  }
-  $('.sign-off').click(singOff);
+  window.location.href = '../index.html';
+}
+$('.sign-off').click(singOff);
   
-  function next() {
-    window.location.href = '../views/we.html';
-  }
-  $('.we').click(next);
+function next() {
+  window.location.href = '../views/we.html';
+}
+$('.we').click(next);
 
-  function backico() {
-    window.location.href = '../views/home.html';
-  }
-  $('.backico').click(backico);
+function backico() {
+  window.location.href = '../views/home.html';
+}
+$('.backico').click(backico);
   
--- js/search.js
+++ js/search.js
@@ -1,23 +1,23 @@
 var searchBtn = $('#searchMovie');
 
-searchBtn.on('click', function (event) {
+searchBtn.on('click', function(event) {
   var inputFld = $('#inputMovie').val();
-  localStorage.setItem("title", inputFld);
-  var title = localStorage.getItem("title");
+  localStorage.setItem('title', inputFld);
+  var title = localStorage.getItem('title');
   console.log(title);
   handleSearchBtn();
 });
 
 
 function handleSearchBtn() {
-    console.log(localStorage.getItem("title"));
-    var url = 'https://www.omdbapi.com/?&apikey=8ad4c53d&s=' + encodeURI(localStorage.getItem("title"));
-    $.ajax({
-      url: url,
-      success: renderMovies
-    });
-  };
-  handleSearchBtn();
+  console.log(localStorage.getItem('title'));
+  var url = 'https://www.omdbapi.com/?&apikey=8ad4c53d&s=' + encodeURI(localStorage.getItem('title'));
+  $.ajax({
+    url: url,
+    success: renderMovies
+  });
+};
+handleSearchBtn();
 
 
 function renderMovies(response) {
@@ -26,16 +26,15 @@ function renderMovies(response) {
   movieList.empty();
   for (var i in response.Search) {
     var movie = response.Search[i];
-    var div = $('<div class="col">');
-    var img = $('<img  id="poster" src="' + movie.Poster + '" width="120px">');
+    var div = $('<div class="box-movie col-xs-6 col-md-4">');
+    var img = $('<img  id="poster" src="' + movie.Poster + '" width="220px">');
     div.append(img);
-    div.append('<span>' + movie.Title + '</span>');
-    div.append('<button  data-imdb=' + movie.imdbID + ' data-poster=' + movie.Poster + ' data-year=' + movie.Year + '  data-Title=' + movie.Title + '  type="button" class="btn btn-fav">favoritos</button>');
+    div.append('<br><span class="movietitle">' + movie.Title + '</span>');
+    div.append('<br><button  data-imdb=' + movie.imdbID + ' data-poster=' + movie.Poster + ' data-year=' + movie.Year + '  data-Title=' + movie.Title + '  type="button" class="btn btn-fav btn-danger">Mi Favorito</button>');
     movieList.append(div);
-
   }
-  //guardar las peliculas vistas por el usuario en firebase
-  $('.btn-fav').click(function () {
+  // guardar las peliculas vistas por el usuario en firebase
+  $('.btn-fav').click(function() {
     var imdbID = $(this).data('imdb');
     var titleMovie = $(this).data('title');
     var posterMovie = $(this).data('poster');
@@ -44,10 +43,10 @@ function renderMovies(response) {
     console.log(titleMovie);
     console.log(posterMovie);
     console.log(ratingMovie);
-    firebase.auth().onAuthStateChanged(function (user) {
+    firebase.auth().onAuthStateChanged(function(user) {
       var uid = user.uid;
       Addmoviesseen(imdbID, uid, titleMovie, posterMovie, ratingMovie);
-      alert("Se ha añadido a su seccion favoritos");
+      alert('Se ha añadido a su seccion favoritos');
     });
   });
 }
